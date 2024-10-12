48a814af09983aeaf27dfd74d1a1b29ca14c59eb
natlan82 <natlan82@yahoo.gr>
2024-10-12 13:46:00 +0300

    update 3.3

--- first project.py
+++ first project.py
@@ -7,3 +7,9 @@ print(0o123)
 print("Tell me anything...")
 anything = input()
 print("Hmm...", anything, "... Really?")
+
+counter = 5
+while counter != 0:
+    print("Inside the loop.", counter)
+    counter -= 1
+print("Outside the loop.", counter)
